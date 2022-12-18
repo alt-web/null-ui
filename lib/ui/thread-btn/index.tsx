5dd3b2096fa98d50704fe9e937ffb1174a7f29c9
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-12-18 22:31:29 +0500

    Fix attachment selection on board page

--- lib/ui/thread-btn/index.tsx
+++ lib/ui/thread-btn/index.tsx
@@ -5,15 +5,15 @@ import styles from "./index.module.css"
 
 export function ThreadBtn({data}: {data: ThreadAPI}) {
     return (
-        <Link href={`/threads/${data.id}`}>
             <div className={styles.btn}>
-                <div>#{data.id}</div>
+                <Link href={`/threads/${data.id}`}>
+                    <div>#{data.id}</div>
+                </Link>
                 <div>{data.body}</div>
                 <div className={styles.attachments}>
                     {data.attachments.map(attachment =>
                         <Attachment key={attachment.id} data={attachment} />)}
                 </div>
             </div>
-        </Link>
     )
 }
