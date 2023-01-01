32615ea1b546c2809bb691e35a7fa269f6f6c404
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-01-01 15:39:30 +0500

    Improve reply card style

--- lib/ui/reply-card/index.tsx
+++ lib/ui/reply-card/index.tsx
@@ -1,4 +1,6 @@
-import { ReplyAPI } from "lib/brain"
+import { useState, useEffect } from "react"
+import { FiHash, FiCalendar } from "react-icons/fi"
+import { ReplyAPI, AttachmentAPI } from "lib/brain"
 import { Attachment } from "../attachment"
 import styles from "./index.module.css"
 
@@ -6,11 +8,41 @@ export function ReplyCard({data}: {data: ReplyAPI}) {
     return (
         <div className={styles.reply}>
             <div className={styles.heading}>
-                <div>#{data.id}</div>
-                <div>{new Date(data.created_at).toLocaleString("en")}</div>
+                <div>
+                    <FiHash />{data.id}
+                </div>
+                <DateTime createdAt={data.created_at} />
             </div>
-            <div>{data.body}</div>
-            {data.attachments.map(attachment => <Attachment key={attachment.id} data={attachment} />)}
+            <p>{data.body}</p>
+            <Attachments data={data.attachments} />
+        </div>
+    )
+}
+
+// Shows date and time without hydration errors
+function DateTime({createdAt}: {createdAt: string}) {
+    const dateObj = new Date(createdAt)
+    const enString = dateObj.toLocaleString("en")
+    const [date, setDate] = useState(enString)
+
+    useEffect(() => {
+        const localeString = dateObj.toLocaleString()
+        setDate(localeString)
+    }, [])
+
+    return (
+        <div>
+            <FiCalendar />{date}
+        </div>
+    )
+}
+
+function Attachments({data}: {data: AttachmentAPI[]}) {
+    if (data.length < 1) return <></>
+    return (
+        <div className={styles.attachments}>
+            {data.map(attachment =>
+                <Attachment key={attachment.id} data={attachment} />)}
         </div>
     )
 }
