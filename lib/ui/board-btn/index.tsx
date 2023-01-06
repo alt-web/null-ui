89ad8cac19a7498fffd8a002e8f8d121ce97e56c
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-01-06 17:01:50 +0500

    Improve board button design
    Add shadow, animation when changing color, and remove unnecessary elements.

--- lib/ui/board-btn/index.tsx
+++ lib/ui/board-btn/index.tsx
@@ -1,14 +1,14 @@
-import { BoardAPI } from "lib/brain"
 import Link from "next/link"
+import { BoardAPI } from "lib/brain"
 import styles from "./index.module.css"
 
-export const BoardBtn = ({data}: {data: BoardAPI}) => {
-    return (
-        <Link href={`/boards/${data.code}`}>
-            <div className={styles.btn}>
-                <div className={styles.code}>/{data.code}/</div>
-                <h4 className={styles.title}>{data.name}</h4>
-            </div>
-        </Link>
-    )
-}
+// A simple button with a link to the board page
+// ------------------------
+// | code | name          |
+// ------------------------
+export const BoardBtn = ({data}: {data: BoardAPI}) => (
+    <Link href={`/boards/${data.code}`} className={styles.btn}>
+        <h3 className={styles.code}>{data.code}</h3>
+        <div className={styles.title}>{data.name}</div>
+    </Link>
+)
